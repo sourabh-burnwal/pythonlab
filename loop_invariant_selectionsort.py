088b23d2d7ee84360296a38c2b3dd7c8ba2638bd
Soami Charan <charansoami@gmail.com>
2019-11-08 22:05:01 +0530

    Update loop_invariant_selectionsort.py

--- loop_invariant_selectionsort.py
+++ loop_invariant_selectionsort.py
@@ -5,17 +5,17 @@
 import random
 
 #inner loop invariant function
-def innerinv(alist,i,j,smallest):
-    for k in range(i+1,j-1):
-        if(alist[smallest]<alist[k]): #inner loop invariant expression
+def innerinv(alist, i, j, smallest):
+    for k in range(i + 1,j - 1):
+        ifalist[smallest] < alist[k]: #inner loop invariant expression
             print("inner loop invariant verified")
         else:
             print("inner loop invariant not verified")
 
 #outer loop invariant function
-def outerinv(alist,i):
-    for k in range(i,len(alist)-1):
-        if(alist[i-1]<=alist[k]): #outer loop invariant expression
+def outerinv(alist, i):
+    for k in range(i, len(alist) - 1):
+        if alist[i - 1] <= alist[k]: #outer loop invariant expression
             print("outer loop invariant verified")
         else:
             print("outer loop invariant not verified")
@@ -23,24 +23,24 @@ def outerinv(alist,i):
 #function to do selection sort
 def selectionsort(alist):
     for i in range(len(alist)):
-        smallest= i
-        for j in range(i+1,len(alist)):
-            if(alist[smallest]>alist[j]):
-                smallest=j
+        smallest = i
+        for j in range(i + 1, len(alist)):
+            if(alist[smallest] > alist[j]):
+                smallest = j
                 #check if inner loop invariant verified
-                innerinv(alist,i,j,smallest)
-        alist[i],alist[smallest]= alist[smallest],alist[i]
-        if(i>0):
+                innerinv(alist, i, j, smallest)
+        alist[i], alist[smallest] = alist[smallest], alist[i]
+        if(i > 0):
             #check if outer loop invariant verified
-            outerinv(alist,i)
+            outerinv(alist, i)
 
 #driver function
-alist=[]
-for i in range(0,8):
-    x= random.randint(1,20)
+alist = []
+for i in range(0, 8):
+    x = random.randint(1, 20)
     alist.append(x)
-print("unsorted list is: ", end=" ")
+print("unsorted list is: ", end = " ")
 print(alist)
 selectionsort(alist)
-print("sorted list is: ", end=" ")
+print("sorted list is: ", end = " ")
 print(alist)
